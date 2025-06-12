35f69d7c07ad0d75f913e519e6997d4932c6871c
Benjamín Maureira Flores <benmauflo@alum.us.es>
2025-06-12 17:17:05 +0200

    fix: corregidos falsos negativos

--- src/evaluators/json_evaluator.py
+++ src/evaluators/json_evaluator.py
@@ -21,7 +21,5 @@ def evaluate_json(expected_result, generated_result):
     # Extraer y comparar los campos 'pattern'
     expected_pattern = expected_result.get("pattern", "").strip().lower()
     generated_pattern = generated_result.get("pattern", "").strip().lower()
-    if('method' in generated_pattern):
-        generated_pattern = generated_pattern.replace('method', '').strip()
     
-    return 'pass' if expected_pattern == generated_pattern else 'fail'
+    return 'pass' if expected_pattern in generated_pattern else 'fail'
